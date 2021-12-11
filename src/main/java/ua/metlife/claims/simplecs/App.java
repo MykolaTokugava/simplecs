94a971d6be9701720d11ca90b29e10fa3739482f
Mykola <ender.ukrid@gmail.com>
2021-12-11 14:07:42 +0200

    -

--- src/main/java/ua/metlife/claims/simplecs/App.java
+++ src/main/java/ua/metlife/claims/simplecs/App.java
@@ -1,15 +1,9 @@
 package ua.metlife.claims.simplecs;
 
-import com.citi.gsp.ds.snl.dowloader.Loader;
-import com.sun.javafx.util.Logging;
-import com.sun.org.apache.bcel.internal.util.ClassLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.ArrayList;
 
 
 @SpringBootApplication
@@ -19,8 +13,6 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        Loader.testLoader();
-
     }
 
 
