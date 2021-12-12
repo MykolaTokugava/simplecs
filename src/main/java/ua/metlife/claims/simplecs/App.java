31645288c6107a7c25694d70b61699342eb78252
nam <ender.ukrid@gmail.com>
2021-12-12 17:16:06 +0200

    test

--- src/main/java/ua/metlife/claims/simplecs/App.java
+++ src/main/java/ua/metlife/claims/simplecs/App.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
+//ssh git
+//https://www.youtube.com/watch?v=MnU1U7GCWLk
 @SpringBootApplication
 @EnableJpaRepositories("ua.metlife.claims.simplecs.repo")
 @EntityScan(basePackages = "ua.metlife.claims.simplecs")
@@ -14,9 +15,6 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
-
-
 }
 
 
